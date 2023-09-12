eac4fa22eb0f967380a780ea6211844cf855890c
Michal Majka <michalmajka@hotmail.com>
2023-09-12 20:32:56 +0200

    Fixes #3

--- R/easyPlot.R
+++ R/easyPlot.R
@@ -1,14 +1,17 @@
-#' Launch easyPlot interactive User Interface
+#' Start an interactive User Interface easyPlot
 #'
-#' @param data an optional data frame with at least two columns passed as a character string
-#' @details
-#' easyPlot()
+#' @param data (character) an optional data frame with at least two columns passed as a character string
 #'
-#' data(iris)
+#' @author Michal Majka
 #'
-#' easyPlot(data = "iris")
+#' @import shiny
+#'
 #' @export
-#' @importFrom shiny runApp
+#'
+#' @examples
+#' # easyPlot()
+#'
+#' # easyPlot(data = "iris")
 
 easyPlot <- function(data = "FALSE") {
 
@@ -17,23 +20,41 @@ easyPlot <- function(data = "FALSE") {
   if (appDir == "")
     stop("Could not find directory. Try re-installing 'easyPlot'.", call. = FALSE)
 
+  # Makes sure all dependencies are available
+  dependencies <- c("ggplot2", "shinyjs", "shinyAce", "shinyBS",
+                    "shinythemes", "Cairo", "DT", "colourpicker")
+
+  ind_missing_package <- !dependencies %in% utils::installed.packages()[ ,"Package"]
+
+  if (any(ind_missing_package)) {
+      n_missing_packages <- sum(ind_missing_package)
+      stop("Please install ",
+           ifelse(n_missing_packages == 1, "the", "these"),
+           " missing package",
+           ifelse(n_missing_packages > 1, "s", ""),
+           ":\n ",
+           "install.packages(",
+           ifelse(n_missing_packages > 1, "c(", ""),
+           paste0("\"", dependencies[ind_missing_package], collapse = "\", "),
+           ifelse(n_missing_packages > 1, "\"))", "\")"))
+  }
+
   if (!is.character(data))
-    stop("Input has to be a character string with the name of a data frame")
+    stop("easyPlot(): ", "Input has to be a character string with the name of a data frame", call. = FALSE)
 
   if (data != "FALSE") {
 
-    if (!is.data.frame(get(data)) )
-      stop("Please provide the name of a data frame with at least two columns")
-
-    if (length(get(data)) < 2)
-      stop("Please provide the name of a data frame with at least two columns")
+    if (!is.data.frame(get(data)) | length(get(data)) < 2)
+      stop("easyPlot(): ", "Please provide the name of a data frame with at least two columns", call. = FALSE)
   }
 
   # After closing the app these variables will be automatically removed
   assign(".easyPlotEnv", new.env(), envir = .GlobalEnv)
   assign("name", data, envir = .easyPlotEnv)
 
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir,
+                display.mode = "normal",
+                launch.browser = TRUE)
 
 }
 
